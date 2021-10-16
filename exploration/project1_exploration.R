658be7ee6087e9becf25fcd9fc85c9688bdf78b5
atramirez <adenramirez166@gmail.com>
2021-10-16 13:09:48 -0700

    added initial cleaning, joins for tables

--- exploration/project1_exploration.R
+++ exploration/project1_exploration.R
@@ -16,6 +16,7 @@ flightsdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=flights.
 airportsdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=airports.csv")
 airlinesdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=airlines.csv")
 
+##########initial flightsdf cleaning##########
 #this will set all the na values in each delay reason column to 0 for future addition and math to be done with these
 flightsdf <- flightsdf %>% 
   mutate(AIR_SYSTEM_DELAY = replace(AIR_SYSTEM_DELAY, is.na(AIR_SYSTEM_DELAY), 0)) %>%
@@ -24,6 +25,20 @@ flightsdf <- flightsdf %>%
   mutate(LATE_AIRCRAFT_DELAY = replace(LATE_AIRCRAFT_DELAY, is.na(LATE_AIRCRAFT_DELAY), 0)) %>%
   mutate(WEATHER_DELAY = replace(WEATHER_DELAY, is.na(WEATHER_DELAY), 0))
 
-head(flightsdf)
-head(airportsdf)
-head(airlines)
+#this will look at any departure times and arrivals times missing and drop the row if there are
+#since these are very important to analize without them it is more useless
+flightsdf <- flightsdf %>% drop_na(DEPARTURE_TIME, ARRIVAL_TIME)
+
+##########Initial airportsdf cleaning##########
+#checks lat/long for na values and removes the row
+airportsdf <- airportsdf %>% drop_na(LATITUDE, LONGITUDE)
+
+#no cleaning needed for airlinesdf
+
+#post initial clean count
+count(flightsdf)
+
+##########Joining tables for airports and airlines#########
+flightsdf <- flightsdf %>% left_join(airportsdf, by = c("ORIGIN_AIRPORT" = "IATA_CODE"))
+flightsdf <- flightsdf %>% left_join(airportsdf, by = c("ORIGIN_AIRPORT" = "IATA_CODE"))
+flightsdf <- flightsdf %>% left_join(airlinesdf, by = c("AIRLINE" = "IATA_CODE"))
