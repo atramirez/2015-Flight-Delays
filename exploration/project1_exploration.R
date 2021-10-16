85ae366926b12945443cb3b9c8d32f68ba39e3cf
atramirez <adenramirez166@gmail.com>
2021-10-16 00:10:31 -0700

    added mutate commands for na values in reasons

--- exploration/project1_exploration.R
+++ exploration/project1_exploration.R
@@ -16,6 +16,14 @@ flightsdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=flights.
 airportsdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=airports.csv")
 airlinesdf = read_csv("https://www.kaggle.com/usdot/flight-delays?select=airlines.csv")
 
+#this will set all the na values in each delay reason column to 0 for future addition and math to be done with these
+flightsdf <- flightsdf %>% 
+  mutate(AIR_SYSTEM_DELAY = replace(AIR_SYSTEM_DELAY, is.na(AIR_SYSTEM_DELAY), 0)) %>%
+  mutate(SECURITY_DELAY = replace(SECURITY_DELAY, is.na(SECURITY_DELAY), 0)) %>%
+  mutate(AIRLINE_DELAY = replace(AIRLINE_DELAY, is.na(AIRLINE_DELAY), 0)) %>%
+  mutate(LATE_AIRCRAFT_DELAY = replace(LATE_AIRCRAFT_DELAY, is.na(LATE_AIRCRAFT_DELAY), 0)) %>%
+  mutate(WEATHER_DELAY = replace(WEATHER_DELAY, is.na(WEATHER_DELAY), 0))
+
 head(flightsdf)
 head(airportsdf)
 head(airlines)
